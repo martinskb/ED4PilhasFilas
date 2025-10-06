5087c5f389e2d27d3d23c428dedc772db05e43bf
kaio <kaio22barra@gmail.com>
2025-10-06 09:16:50 -0300

    Update Exercicios123.java

--- src/Exercicios123.java
+++ src/Exercicios123.java
@@ -7,3 +7,6 @@
 //Acho melhor usar uma pilha, porque os exames chegam em uma ordem, mas no final precisam ser impressos de trás pra frente. Se eu já souber o número máximo de exames, posso usar um array, mas se a quantidade variar muito, é melhor usar uma lista encadeada, já que ela cresce conforme precisa e não desperdiça espaço.
 
 //Exercício 3
+//Exercício 3
+//Um sistema recebe tarefas numa fila, mas as mais urgentes vão pra uma pilha de prioridade. Qual é a ordem final de execução?
+//Primeiro o sistema faz as tarefas da pilha de prioridades, porque são as mais urgentes e seguem a regra LIFO. Quando a pilha estiver vazia, ele volta pra fila normal e faz as tarefas na ordem que chegaram, seguindo a regra FIFO. Então, primeiro vão as urgentes (pilha) e depois as normais (fila).
