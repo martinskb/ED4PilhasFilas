71359f6a46a03e84f0f8c07bdcca62706f2a2a27
kaio <kaio22barra@gmail.com>
2025-10-06 09:15:05 -0300

    Initial commit

---
+++ src/Exercicios123.java
@@ -0,0 +1,9 @@
+//Exercício 1
+//A função Desfazer (Ctrl+Z) é mais parecida com uma fila ou uma pilha?
+//É uma pilha, porque o último comando que eu fiz é o primeiro que vai ser desfeito. Ou seja, ela segue a regra LIFO, o último a entrar é o primeiro a sair.
+
+//Exercício 2
+//Um laboratório precisa registrar exames na ordem de chegada e, no final do dia, imprimir na ordem inversa. Que estrutura deve usar? Deve ser com vetor (array) ou lista encadeada?
+//Acho melhor usar uma pilha, porque os exames chegam em uma ordem, mas no final precisam ser impressos de trás pra frente. Se eu já souber o número máximo de exames, posso usar um array, mas se a quantidade variar muito, é melhor usar uma lista encadeada, já que ela cresce conforme precisa e não desperdiça espaço.
+
+//Exercício 3
---
+++ src/Pilha.java
@@ -0,0 +1,57 @@
+public class Pilha {
+    int topo;
+    int tamanho;
+    int elementos[];
+
+    public Pilha(int tamanho) {
+        this.tamanho = tamanho;
+        elementos = new int[tamanho];
+        topo = -1;
+    }
+
+    public void push(int valor) {
+        if (topo == tamanho - 1) {
+            System.out.println("Pilha cheia!");
+        } else {
+            topo++;
+            elementos[topo] = valor;
+        }
+    }
+
+    public int pop() {
+        if (topo == -1) {
+            System.out.println("Pilha vazia!");
+            return -1;
+        } else {
+            int valor = elementos[topo];
+            topo--;
+            return valor;
+        }
+    }
+
+    public void mostrarTopo() {
+        if (topo == -1) {
+            System.out.println("Pilha vazia!");
+        } else {
+            System.out.println("Topo: " + elementos[topo]);
+        }
+    }
+
+    public boolean vazia() {
+        return topo == -1;
+    }
+
+    public static void main(String[] args) {
+        Pilha p = new Pilha(5);
+
+        p.push(10);
+        p.push(20);
+        p.push(30);
+
+        p.mostrarTopo(); // Mostra o topo
+
+        while (!p.vazia()) {
+            System.out.println("Removendo: " + p.pop());
+        }
+    }
+}
